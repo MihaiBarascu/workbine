fe7787c49e5a06eb453621540cd335ada25bb838
Mihai-Alexandru Barascu <mihaialex_barascu@yahoo.com>
2026-09-10 21:12:01 +0300

    feat: add goal routes

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,17 @@
 <?php
 
+use App\Http\Controllers\GoalController;
 use Illuminate\Support\Facades\Route;
 
-Route::inertia('/', 'welcome')->name('home');
+Route::get('/', [GoalController::class, 'index'])->name('home');
+Route::get('goals', [GoalController::class, 'index'])->name('goals.index');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::inertia('dashboard', 'dashboard')->name('dashboard');
+    Route::get('goals/create', [GoalController::class, 'create'])->name('goals.create');
+    Route::post('goals', [GoalController::class, 'store'])->name('goals.store');
 });
+
+Route::get('goals/{goal}', [GoalController::class, 'show'])->name('goals.show');
 
 require __DIR__.'/settings.php';
