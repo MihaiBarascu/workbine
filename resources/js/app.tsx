40a9ece7e004ac568fbf13701f90b3910e9a998f
Mihai-Alexandru Barascu <mihaialex_barascu@yahoo.com>
2026-09-10 21:13:52 +0300

    feat: render public goal pages without app shell

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -12,7 +12,7 @@ void createInertiaApp({
     title: (title) => (title ? `${title} - ${appName}` : appName),
     layout: (name) => {
         switch (true) {
-            case name === 'welcome':
+            case name === 'welcome' || name === 'goals/index' || name === 'goals/show':
                 return null;
             case name.startsWith('auth/'):
                 return AuthLayout;
@@ -36,5 +36,4 @@ void createInertiaApp({
     },
 });
 
-// This will set light / dark mode on load...
 initializeTheme();
