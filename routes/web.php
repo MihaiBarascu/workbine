6d6dbbf713d08ffed1be9ed6ea46ac0f0151d2f7
Mihai-Alexandru Barascu <mihaialex_barascu@yahoo.com>
2026-09-10 21:17:16 +0300

    fix: prioritize goal creation route

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [GoalController::class, 'index'])->name('home');
 Route::get('goals', [GoalController::class, 'index'])->name('goals.index');
-Route::get('goals/{goal}', [GoalController::class, 'show'])->name('goals.show');
 
 Route::middleware('guest')->group(function () {
     Route::get('auth/google', [GoogleAuthController::class, 'redirect'])->name('google.redirect');
@@ -18,5 +17,7 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('goals/create', [GoalController::class, 'create'])->name('goals.create');
     Route::post('goals', [GoalController::class, 'store'])->name('goals.store');
 });
+
+Route::get('goals/{goal}', [GoalController::class, 'show'])->name('goals.show');
 
 require __DIR__.'/settings.php';
