8f82d8a57db120247c5378ec13549409af776f63
Mihai-Alexandru Barascu <mihaialex_barascu@yahoo.com>
2026-09-10 21:11:43 +0300

    feat: validate goal creation

---
+++ app/Http/Requests/StoreGoalRequest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreGoalRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /** @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string> */
+    public function rules(): array
+    {
+        return [
+            'title' => ['required', 'string', 'max:160'],
+            'description' => ['nullable', 'string', 'max:5000'],
+        ];
+    }
+}
