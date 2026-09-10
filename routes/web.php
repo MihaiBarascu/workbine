091a8d82119cc5500a089dd248474ddf92726335
Mihai-Alexandru Barascu <mihaialex_barascu@yahoo.com>
2026-09-10 21:16:15 +0300

    feat: add Google auth routes

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,22 @@
 <?php
 
+use App\Http\Controllers\Auth\GoogleAuthController;
 use App\Http\Controllers\GoalController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [GoalController::class, 'index'])->name('home');
 Route::get('goals', [GoalController::class, 'index'])->name('goals.index');
+Route::get('goals/{goal}', [GoalController::class, 'show'])->name('goals.show');
+
+Route::middleware('guest')->group(function () {
+    Route::get('auth/google', [GoogleAuthController::class, 'redirect'])->name('google.redirect');
+    Route::get('auth/google/callback', [GoogleAuthController::class, 'callback'])->name('google.callback');
+});
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::inertia('dashboard', 'dashboard')->name('dashboard');
     Route::get('goals/create', [GoalController::class, 'create'])->name('goals.create');
     Route::post('goals', [GoalController::class, 'store'])->name('goals.store');
 });
-
-Route::get('goals/{goal}', [GoalController::class, 'show'])->name('goals.show');
 
 require __DIR__.'/settings.php';
