492c5da3cf339d0633154b23373e40b8c78ccd19
Mihai-Alexandru Barascu <mihaialex_barascu@yahoo.com>
2026-09-10 22:16:55 +0300

    refactor: relate users to topics

--- app/Models/User.php
+++ app/Models/User.php
@@ -30,7 +30,7 @@ use Laravel\Fortify\TwoFactorAuthenticatable;
  * @property string|null $remember_token
  * @property Carbon|null $created_at
  * @property Carbon|null $updated_at
- * @property-read Collection<int, Goal> $goals
+ * @property-read Collection<int, Topic> $topics
  */
 #[Fillable(['name', 'email', 'google_id', 'avatar', 'password'])]
 #[Hidden(['password', 'two_factor_secret', 'two_factor_recovery_codes', 'remember_token'])]
@@ -39,10 +39,10 @@ class User extends Authenticatable implements PasskeyUser
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable, PasskeyAuthenticatable, TwoFactorAuthenticatable;
 
-    /** @return HasMany<Goal, $this> */
-    public function goals(): HasMany
+    /** @return HasMany<Topic, $this> */
+    public function topics(): HasMany
     {
-        return $this->hasMany(Goal::class);
+        return $this->hasMany(Topic::class);
     }
 
     /**
