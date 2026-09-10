0e991f7ae7823144ced0f7faeeb91946ead21934
Mihai-Alexandru Barascu <mihaialex_barascu@yahoo.com>
2026-09-10 22:15:03 +0300

    refactor: introduce topics

---
+++ app/Models/Topic.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Models;
+
+use Database\Factories\TopicFactory;
+use Illuminate\Database\Eloquent\Attributes\Fillable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Carbon;
+
+/**
+ * @property int $id
+ * @property int $user_id
+ * @property string $title
+ * @property string $slug
+ * @property string|null $description
+ * @property Carbon|null $created_at
+ * @property Carbon|null $updated_at
+ * @property-read User $user
+ */
+#[Fillable(['user_id', 'title', 'slug', 'description'])]
+class Topic extends Model
+{
+    /** @use HasFactory<TopicFactory> */
+    use HasFactory;
+
+    /** @return BelongsTo<User, $this> */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function getRouteKeyName(): string
+    {
+        return 'slug';
+    }
+}
